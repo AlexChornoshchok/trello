a9d100e2975e8090074e23c30171a0c3b4c2aa9a
F-18-02 <1mail@mail.ru>
2018-09-01 13:29:56 +0300

    01.09.2018

--- js/script.js
+++ js/script.js
@@ -1,50 +1,76 @@
 (function () {
 
-  // var  listCardsHTML = [
-  //   '<div class="header"> <h3>Titel</h3> </div>',
-  //   '<div class="footer" onclick = "addCard()"> Add card</div>'
-  //   ];
-    var  listCardsHTML =`
-      <div class="header"> <h3>Titel</h3> </div>
-      <div class="footer" onclick = "addCard()"> Add card</div>
-      `;
+  // var  listCardsHTML = `
+  //   <div class="header"> <h4> Tital </h4> </div>
+  //   <div class="footer" onclick = "addCard()"> Add card</div>
+  //   `;
+
+  var  listCardsHTML = function(groupName) {
+    return `
+      <div class="header"> <h4> Tital </h4> </div>
+      <div class="footer" onclick = "addCard('${groupName}')"> Add card</div>
+    `;
+  }
     
   // var defaultCard = '<div class = "card">Card</div>';
   var defaultCard = document.createElement('div');
   defaultCard.className = "card";
-  defaultCard.innerText = "Card"; 
+  defaultCard.draggable = true;
+  defaultCard.ondragstart = function(event){
+    console.log(event);
 
-    // var defaultCard = `
-    // <div class="card">
-    //     <div class="card-header">
-    //        <progress max="100" value="10">Progress bar</progress>
-    //        <span>X</span>
-    //     </div>
-    //     <div class = "card-title">
-    //       <h4>Titel</h4>
-    //     </div>
-    //     <div class = "card-footer">
-    //       <button class = "card-edit">
-    //         ...
-    //       </button> 
-    //       <div class = "card-status">  
-    //       </div> 
-    //         <div class = "card-date"> Date </div>       
-    //           <img class = "card-img" src="" alt="avatar">
-    //         </div>    
-    //       </div>
-    // </div>      
-    // `;
-
-  var group = document.getElementsByClassName('group')[0];
-  group.innerHTML = listCardsHTML;
+  }
+  // defaultCard.innerText = "Card"; 
+  defaultCard.innerHTML = `
+    <div class="card">
+        <div class="card-header">
+           <progress max="100" value="10">Progress bar</progress>
+           <span>X</span>
+        </div>
+        <div class = "card-title">
+          <h4>Tital</h4>
+        </div>
+        <div class = "card-footer">
+          <button class = "card-edit">
+            ...
+          </button> 
+          <div class = "card-status">  
+          </div> 
+            <div class = "card-date"> Date </div>       
+              <img class = "card-img" src="" alt="avatar">
+            </div>    
+          </div>
+    </div>      
+    `;
+ 
+  // var group = document.getElementsByClassName('group')[0];
+  // group.innerHTML = listCardsHTML;
+  var group1 = document.getElementsByClassName('group-1')[0];
+  group1.innerHTML = listCardsHTML('group-1');
+  var group2 = document.getElementsByClassName('group-2')[0];
+  group2.innerHTML = listCardsHTML('group-2');
   //var listCards = document.getElementsByClassName('card');
 
-  window.addCard = function () {
-    group.appendChild(defaultCard);
-    
-    // listCardsHTML.splice(listCardsHTML.length - 1, 0, defaultCard);
-    //group.innerHTML = listCardsHTML;
-    // listCards.insertBefore(defaultCard, listCards.lastChild);
+  window.addCard = function (type) {
+    console.log(type);
+    let group = document.getElementsByClassName(type)[0];
+    console.log(group.outerHTML);
+    let listCards = group.outerHTML;
+    console.log(listCards instanceof Array);
+   // console.log(listCards);
+    listCards.splice(listCards.length - 1, 0, defaultCard);
+    group.innerHTML = listCards;
+  //  listCardsHTML.splice(listCardsHTML.length - 1, 0, defaultCard);
+  //group.innerHTML = listCardsHTML;
+  };
+
+  window.cardDrop = function(event) {
+    console.log(event);
+  
+  };
+
+  window.onDragOver = function(event) {
+    event.preventDefault();
+
   };
 }());
